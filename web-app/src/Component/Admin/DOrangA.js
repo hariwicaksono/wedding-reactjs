aee47979270a534ae415453bf914daa8e8c6442e
Hari Wicaksono <hariwicaksono87@gmail.com>
2020-06-03 22:20:06 +0700

    Menambah Fungsi Pagination

--- web-app/src/Component/Admin/DOrangA.js
+++ web-app/src/Component/Admin/DOrangA.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {Pagination, PageItem} from 'react-bootstrap'
 
 class DOrangA extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          currentPage: props.currentPage
+        };
+      }
+      handleClick = (e, index) => {
+        e.preventDefault();
+        console.log(index);
+        this.setState({
+          currentPage: index
+        });
+      };
     render() {
-        const lisOrang = this.props.data.map(data => (
+        const { pageSize, pageCount, data } = this.props;
+        const { currentPage } = this.state;
+    
+        let pageNumbers = [];
+        for (let i = 0; i < pageCount; i++) {
+          pageNumbers.push(
+            <Pagination.Item key={i} active={currentPage === i ? true : false} onClick={e => this.handleClick(e, i)} href="#">
+                {i + 1}
+            </Pagination.Item>
+          );
+        }
+        const paginatedData = data.slice(
+          currentPage * pageSize,
+          (currentPage + 1) * pageSize
+        );
+
+        const lisOrang = paginatedData.map(data => (
             <tbody key={data.id} >
                 <tr>
                     <td> {data.nama} </td>
@@ -21,6 +52,11 @@ class DOrangA extends Component {
         ))
         return (
             <div>
+              <Pagination>
+               
+                    {pageNumbers}
+                  
+            </Pagination>
                 <table className="table table-hover">
                     <thead>
                         <tr>
--- web-app/src/Component/Admin/OrangA.js
+++ web-app/src/Component/Admin/OrangA.js
@@ -5,15 +5,12 @@ import API from '../../ServiceApi/Index'
 import DOrangA from './DOrangA'
 import {Container} from 'react-bootstrap'
 //import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import Pagination from 'react-js-pagination'
 
 class OrangA extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            orang: [],
-            filteredUser: [],
-            activePage : 1
+            orang: []
         }
     }
 
@@ -35,7 +32,6 @@ class OrangA extends Component {
         API.GetOrang().then(res => {
             this.setState({
                 orang: res
-
             })
         })
     }
@@ -44,23 +40,7 @@ class OrangA extends Component {
         this.getHandler()
     }
 
-    handlerChange = (pageNumber) =>{
-        console.log(pageNumber)
-        const filteredUser = this.state.orang;
-    let perPage = 5;
-    const start = pageNumber === 1 ? 0 : pageNumber*perPage-perPage;
-    const end = start + 5;
-    console.log(`${start}, ${end}`);
-    const result = filteredUser.slice(start, end);
-
-    this.setState({
-      activePage: pageNumber,
-      filteredUser:result
-    });
-    }
-
     render() {
-
         return ( 
         <div>
         <NavbarA />
@@ -68,16 +48,8 @@ class OrangA extends Component {
               
                 <Container className="my-3 px-3 py-4" fluid>
                     <h2 className="mb-3">DATA TAMU UNDANGAN <Link className="btn btn-info btn-sm float-right" to="/tproduk" >TAMBAH ORANG</Link></h2>
-                    <Pagination
-                        itemClass="page-item"
-                        linkClass="page-link"
-                        activePage={this.state.activePage}
-                        itemsCountPerPage={5}
-                        totalItemsCount={this.state.orang.length}
-                        pageRangeDisplayed={5}
-                        onChange={this.handlerChange.bind(this)}
-                    />
-                    <DOrangA data={this.state.orang} remove={this.hapus} />
+                    
+                    <DOrangA pageSize={5} pageCount={this.state.orang.length} currentPage={0} data={this.state.orang} remove={this.hapus} />
                     
                 </Container>
             </div>
